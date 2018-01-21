eb4f85ee93cff0b236402eb37815778bff0df7b6
Justin Noel <justin.noel@calendee.com>
2018-01-21 17:31:49 -0600

    end of ngIf

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,66 +6,34 @@ import { Component } from '@angular/core';
   template: `
       <div class="app">
         
-        <button (click)="handleClick(username.value)">
-          Get Value
-        </button>
-        
-        <!--<input -->
-          <!--type="text" -->
-          <!--[value]="name"-->
-          <!--(blur)="handleBlur($event)"-->
-          <!--(input)="handleInput($event)"-->
-        <!--/>-->
-        
-        <div>{{ name }}</div>
-
-        <!--<input-->
-          <!--type="text"-->
-          <!--[ngModel]="name"-->
-          <!--(ngModelChange)="handleChange($event)"-->
-        <!--/>-->
-
-        <!--<input-->
-          <!--type="text"-->
-          <!--[(ngModel)]="name"-->
-        <!--/>-->
-        
         <input
           type="text"
-          #username
-        />
-
+          [value]="name"
+          (input)="handleChange($event.target.value)">
+        
+        <div *ngIf="name.length > 2">
+          Searching for... {{ name }}
+        </div>
+        
+        <div>^^ Is sugar for :</div>
+        
+        <template [ngIf]="name.length > 2">
+          <div>
+            Searching for... {{ name }}
+          </div>
+        </template>
+        
       </div>
   `
 })
 export class AppComponent {
 
-  name : string = "Justin";
+  name : string = '';
 
   constructor() {
   }
 
-  handleClick(value: string) {
-    console.log('Name = ', value);
+  handleChange(value: string) {
+    this.name = value;
   }
-
-
-  // handleClick() {
-  //   this.name = "NOEL!";
-  // }
-
-  // handleBlur(event: any) {
-  //   console.log(event);
-  //   this.name = event.target.value;
-  // }
-
-  // handleInput(event: any) {
-  //   this.name = event.target.value;
-  // }
-
-  // handleChange(value: string) {
-  //   this.name = value;
-  // }
-
 }
-
