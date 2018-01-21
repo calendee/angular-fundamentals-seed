f78a118dbad0b14c2b48c9f5cd0218deb22abc8e
Justin Noel <justin.noel@calendee.com>
2018-01-21 12:18:47 -0600

    end of interpolation and expressions

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,13 +5,23 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
       <div class="app">
-        {{ title }}
+        {{ title + '!'}}
+        <div>
+          {{ numberOne + numberTwo }}
+        </div>
+        
+        <div>
+          {{ isHappy ? ':)' : ':('}}
+        </div>
       </div>
   `
 })
 export class AppComponent {
 
   title: string;
+  numberOne: number = 1;
+  numberTwo: number = 2;
+  isHappy: boolean = true;
 
   constructor() {
     this.title = 'Ultimate Angular1';
