ecf45384e5b911f958d227c1ca3926d91fc32e54
Justin Noel <justin.noel@calendee.com>
2018-01-25 20:14:40 -0600

    end of form validation and error states

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -14,7 +14,17 @@ import { Baggage } from '../../models/baggage.interface';
         <input 
           type="text" 
           name="fullname"
+          minlength="2"
+          maxlength="25"
+          required
+          #fullname="ngModel"
           [ngModel]="detail?.fullname"/>
+        
+        {{fullname.errors | json}}
+        <div *ngIf="fullname.errors?.required && fullname.touched" class="error">
+          Passenger name required
+        </div>
+
       </div>
       
       <div>
@@ -22,8 +32,14 @@ import { Baggage } from '../../models/baggage.interface';
         <input 
           type="number"
           name="id"
+          required
+          #id="ngModel"
           [ngModel]="detail?.id"
         />
+        
+        <div *ngIf="id.errors?.required && id.dirty" class="error">
+          Passenger ID is required
+        </div>
       </div>
       
       <div>
@@ -58,7 +74,9 @@ import { Baggage } from '../../models/baggage.interface';
         </select>
       </div>
       
-      {{form.value | json}}
+      <div>{{form.value | json}}</div>
+      <div>Valid: {{form.valid | json }}</div>
+      <div>Invalid: {{form.invalid | json}}</div>
     </form>
   `
 })
