76907a5a31c4ebeed98801b5aa1a8f026f7d278b
Justin Noel <justin.noel@calendee.com>
2018-01-26 05:27:41 -0600

    end of dynamic navigator with ngFor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Nav {
+  link: string,
+  name: string,
+  exact: boolean
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
@@ -8,16 +14,11 @@ import { Component } from '@angular/core';
         
         <nav class="nav">
           <a
-            routerLink="/"
+            *ngFor="let item of nav"
+            [routerLink]="item.link"
             routerLinkActive="active"
-            [routerLinkActiveOptions]="{ exact: true }">
-            Home
-          </a>
-
-          <a
-            routerLink="/oops"
-            routerLinkActive="active">
-            404
+            [routerLinkActiveOptions]="{ exact: item.exact }">
+            {{ item.name }}
           </a>
         </nav>
         
@@ -26,6 +27,19 @@ import { Component } from '@angular/core';
   `
 })
 export class AppComponent {
+
+  nav: Nav[] = [
+    {
+      link: '/',
+      name: 'Home',
+      exact: true
+    },
+    {
+      link: '/oops',
+      name: '404',
+      exact: false
+    }
+  ];
 
   name : string = '';
 
