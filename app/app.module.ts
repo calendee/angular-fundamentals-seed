47e9e6c32f82329c35aeec03d9b4f372bde2b0f0
Justin Noel <justin.noel@calendee.com>
2018-01-26 04:58:40 -0600

    end of route module routes and outlet

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 import { PassengerDashboardModule } from "./passenger-dashboard/passenger-dashboard.module";
 
+import { HomeComponent } from './home.component';
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     // Angular modules
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes),
 
     // Custom modules
     PassengerDashboardModule
